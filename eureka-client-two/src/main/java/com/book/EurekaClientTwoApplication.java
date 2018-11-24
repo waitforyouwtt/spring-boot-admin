df663c977a5a5725b1b68192aa1e3f7f2b4a62c8
luoxian <15290810931@163.com>
2018-11-24 12:00:51 +0800

    加入turbine

--- eureka-client-two/src/main/java/com/book/EurekaClientTwoApplication.java
+++ eureka-client-two/src/main/java/com/book/EurekaClientTwoApplication.java
@@ -1,22 +1,18 @@
 package com.book;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@SpringBootApplication
-@EnableTurbine
 @EnableHystrixDashboard
 @EnableHystrix
-@EnableAdminServer
 @EnableEurekaClient
-public class AdminServerApplication {
+@SpringBootApplication
+public class EurekaClientTwoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdminServerApplication.class, args);
+		SpringApplication.run(EurekaClientTwoApplication.class, args);
 	}
 }
--- turbine-service/src/main/java/com/book/TurbineServiceApplication.java
+++ turbine-service/src/main/java/com/book/TurbineServiceApplication.java
@@ -3,16 +3,13 @@ package com.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-
-@EnableHystrixDashboard
-@EnableHystrix
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableEurekaClient
+@EnableTurbine
 @SpringBootApplication
-public class EurekaClientOneApplication {
+public class TurbineServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientOneApplication.class, args);
+		SpringApplication.run(TurbineServiceApplication.class, args);
 	}
 }
